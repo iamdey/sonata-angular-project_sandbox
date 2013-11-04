bd95dde20955e6822c1770d2697559c40973a3c0
David Epely <esion99@gmail.com>
2013-11-04 07:16:27 +0100

    init bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Presta\ComposerPublicBundle\PrestaComposerPublicBundle(),
+            new Dey\Sonata\AngularAdminBundle\DeySonataAngularAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Dey/Sonata/AngularAdminBundle/DeySonataAngularAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dey\Sonata\AngularAdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DeySonataAngularAdminBundle extends Bundle
+{
+}
