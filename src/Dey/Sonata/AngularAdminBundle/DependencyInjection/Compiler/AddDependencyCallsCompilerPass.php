905b1db9f249b4b3b9ace5811246bdb76e443b63
David Epely <esion99@gmail.com>
2013-11-19 17:28:42 +0100

    configure sonata angular pool loader

--- src/Dey/Sonata/AngularAdminBundle/DependencyInjection/Compiler/AddDependencyCallsCompilerPass.php
+++ src/Dey/Sonata/AngularAdminBundle/DependencyInjection/Compiler/AddDependencyCallsCompilerPass.php
@@ -9,23 +9,31 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
  * Description of AddDependencyCallsCompilerPass
  *
  * @author dey
+ * @author thomas rabaix
  */
 class AddDependencyCallsCompilerPass extends BaseAddDependencyCallsCompilerPass
 {
-    public function process(ContainerBuilder $container) 
+    public function process(ContainerBuilder $container)
     {
+        $admins = array();
+        
         foreach ($container->findTaggedServiceIds('sonata.admin') as $id => $tags) {
             $definition = $container->getDefinition($id);
             $arguments = $definition->getArguments();
             $refl = new \ReflectionClass($definition->getClass());
-            
+
             //override default sonata controller if it is not user defined or 
             //the admin class is Angular aware
             if (strlen($arguments[2]) == 0 && $refl->isSubclassOf('\Dey\Sonata\AngularAdminBundle\Admin\Admin')) {
                 $definition->replaceArgument(2, 'DeySonataAngularAdminBundle:CRUD');
             }
+
+            $admins[] = $id;
         }
-                
+
+        $routeLoader = $container->getDefinition('dey.sonata.admin.route_loader');
+        $routeLoader->replaceArgument(1, $admins);
+        
         parent::process($container);
     }
 }
